34a68068e318aaacbec9f7a18aed6fea0b69b422
nethmii98 <nethminagodawithane777@gmail.com>
2024-11-21 08:18:41 +0530

    Hyperparameter Tuning

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -6,8 +6,10 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
+from sklearn.preprocessing import LabelEncoder,StandardScaler
 from sklearn.preprocessing import Normalizer, MinMaxScaler
+from sklearn.preprocessing import PowerTransformer
+from scipy.sparse import csr_matrix,hstack
 
 
 from src.exception import CustomException
@@ -23,62 +25,20 @@ class DataTransformationConfig:
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
-
-    def get_data_transformer_object(self):
-
-        '''
-        This function is responsible for data transformation
-
-        '''
-
-
-        try:
-            numerical_columns = ['sqft_living','sqft_lot','sqft_basement','yr_built','lat','long']
-            categorical_columns = ['bedrooms','bathrooms','floors','condition','grade']
-
-            num_pipeline = Pipeline(
-                steps = [
-                    ("imputer",SimpleImputer(strategy="median")),
-                    ("scaler",StandardScaler())
-                ]
-            )
-            cat_pipeline = Pipeline(
-                steps = [
-                  ("imputer",SimpleImputer(strategy="most_frequent")),
-                  ("one_hot_encoder",OneHotEncoder()),
-                  ("scaler",StandardScaler(with_mean=False))  
-                ]
-            )    
-            
-            logging.info("numerical columns standard scaling completed ")
-            logging.info("Categorical columns encoding completed ")
-
-            preprocessor=ColumnTransformer(
-               [
-                  ("num_pipeline",num_pipeline,numerical_columns),
-                  ("cat_pipeline",cat_pipeline,categorical_columns)  
-               ] 
-            )
-
-            return preprocessor
-
-        except Exception as e:
-            raise CustomException(e,sys) 
         
     def initiate_data_transformation(self,train_path,test_path):
         try:
+
+            numerical_columns = ['sqft_living','sqft_lot','sqft_basement','yr_built','lat','long']
+            categorical_columns = ['bedrooms','bathrooms','floors','condition','grade'] 
+           
+
             train_df = pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
 
-            #train_df.drop(columns=['id','date','waterfront','view','sqft_above','yr_renovated','zipcode','sqft_living15','sqft_lot15'],inplace=True)
-            #test_df.drop(columns=['id','date','waterfront','view','sqft_above','yr_renovated','zipcode','sqft_living15','sqft_lot15'],inplace=True)
-
             logging.info("Read train and test data completed")
-            logging.info("Obtaining preprocessing object")
-
-            # preprocessing_obj = self.get_data_transformer_object()
+            
             target_column_name = 'price'
-            numerical_columns = ['sqft_living','sqft_lot','sqft_basement','yr_built','lat','long']
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
@@ -90,12 +50,11 @@ class DataTransformation:
                 f"Applying preprocessing object on training dataframe and testing dataframe."
             )
 
-            # input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df).toarray()
-            # input_feature_test_arr = preprocessing_obj.fit_transform(input_feature_test_df).toarray()
-            scaler = MinMaxScaler()
+            scaler = StandardScaler()
 
             input_feature_train_arr = scaler.fit_transform(input_feature_train_df)
             input_feature_test_arr = scaler.fit_transform(input_feature_test_df)
+
 
             logging.info(f"Shapes after transformation: X_train={input_feature_train_arr.shape}, X_test={input_feature_test_arr.shape}")
 
@@ -105,7 +64,6 @@ class DataTransformation:
 
             logging.info(f"Shape of input_feature_train_arr: {input_feature_train_arr.shape}")
             logging.info(f"Shape of target_feature_train_df: {np.array(target_feature_train_df).shape}")
-            from scipy.sparse import csr_matrix,hstack
 
             target_train_array = target_feature_train_df.values
 
@@ -129,10 +87,6 @@ class DataTransformation:
             logging.info(f"Shape of train_arr: {train_arr.shape}")
             logging.info(f"Shape of test_arr: {test_arr.shape}")
 
-            # save_object(
-            #     file_path=self.data_transformation_config.preprocessor_obj_file_path,
-            #     obj = preprocessing_obj
-            # )
 
             return(
                 train_arr,
